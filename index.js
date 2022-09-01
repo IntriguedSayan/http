62a228f5cc06d78416fcd26a32ffb99b0c66ff8d
IntriguedSayan <sayan.mid590@gmail.com>
2022-09-01 13:06:31 +0530

    l2addded

---
+++ index.js
@@ -0,0 +1,36 @@
+const http = require("http")
+const fs=require("fs")
+
+const server=http.createServer((req,res)=>{
+    // console.log(req.method)
+    // console.log( req.url)
+    if(req.method==="GET"){
+        const log=req.method + " " + req.url+"\n"
+        fs.appendFile("./log.txt",log,(err,data)=>{
+            if(err)console.log(err)
+           
+        })
+        if(req.url==="/"){
+            res.write("WELCOM")
+            res.end("!")
+        }else if(req.url==="/contact"){
+        
+            return res.end("WELCOMe to constacts")
+        }else if(req.url==="/details"){
+            //read from file and store the data
+            //send the data
+            const data=fs.readFileSync("./data.txt",{encoding:"utf-8"})
+            return res.end(data)
+        }
+    }
+    // else if(req.method==="POST"){
+    //     if(req.url==="/adddetails"){
+
+    //     }
+    // }
+
+    // res.write("xyz")
+    // res.end("!")
+})
+
+server.listen(8080)
